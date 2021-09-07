5337febfe4492808a60b4c0d7ee4bb412b097deb
freedom and linux enthusiast <59083599+junguler@users.noreply.github.com>
2021-09-07 07:24:00 +0430

    Update primitive.sh

--- linux/primitive.sh
+++ linux/primitive.sh
@@ -9,7 +9,7 @@ read outExt
 echo "Number of shapes, less is faster but less accurate, start at 100 for a simple image, 500 for more complex:"
 read numShapes 
 
-echo "Convert to (0=combo,1=triangle,2=rect,3=ellipse,4=circle,5=rotatedrect,6=beziers,7=rotatedellipse,8=polygon), only use 1 number:"
+echo "Convert to (0=combo,1=triangle,2=rect,3=ellipse,4=circle,5=rotatedrect,6=beziers,7=rotatedellipse,8=polygon), only use one number:"
 read setMod 
 
 echo "Extra arguments? (leave empty if you don't need it an press inter):"
